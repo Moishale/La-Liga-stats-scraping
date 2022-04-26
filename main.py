58dffb6ebbe604f5f81506653425a29881b8f9d1
BlacKJacK 1 <>
2022-04-26 01:18:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from utils import LaLiga
+
+year  = input("Enter the  Year of the season to show the table")
+
+test = LaLiga(year)
+test.get_table()
+
+if __name__ == "__main__" :
+  test.plot_table()
---
+++ utils/Liga.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import requests
+
+class LaLiga :
+    
+    def __init__(self , year) :
+         self.url = self.url = f"https://www.skysports.com/la-liga-table/{year}"
+         self.rows = None
+         self.__title = "  #      Team |      Pl |  W |D  |L | F |A | GD | Pts |"
+
+    def  get_table(self) :
+        response = requests.get(self.url).text
+        soup = BeautifulSoup(response , "html.parser")
+        row = soup.find("tbody")
+        self.rows = row
+        
+    def plot_table(self) :
+        print(self.__title)
+        for x in self.rows :
+          a = str(x.text)
+          b = { a.replace("\n" , "  ")}
+          for c in b :
+            print(c)
+
+ 
+
+         
